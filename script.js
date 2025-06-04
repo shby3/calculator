b93ada4a03c6f124d635e53dbc6e84bbd1520117
shby3 <ashby.loshin@gmail.com>
2025-06-04 18:05:42 -0400

    Add html to serve as a calculator
    Added calculator body and buttons. Began events
    in js for calculator buttons.

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
-function add(a, b) {
-    return a + b;
+let num1;
+let num2;
+let operator;
+let displayStr = "";
+let curNum = 1;
+
+const display = document.querySelector("#display");
+const numBtns = document.querySelectorAll(".num-btn");
+const operatorBtns = document.querySelectorAll(".operator-btn");
+
+
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+function operate(a, b, operator) {
+    if (operator === '+')
+        return add(a, b);
+    if (operator === '-')
+        return subtract(a, b);
+    if (operator === '*')
+        return multiply(a, b);
+    if (operator === '/')
+        return divide(a, b);
 }
 
-function subtract(a, b) {
-    return a - b;
+// Add event to display clicked numbers
+for (const btn of numBtns) {
+    btn.addEventListener("click", () => {
+        if (displayStr.length <= 10) {
+            displayStr += btn.getAttribute("num");
+            display.textContent = displayStr;
+        }
+    });
 }
 
-function multiply(a, b) {
-    return a * b;
-}
-
-function divide(a, b) {
-    return a / b;
-}
+// Add event to enable operations
